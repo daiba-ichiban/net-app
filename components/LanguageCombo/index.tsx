8f8ce2a8433cd57fbe86b480d8d6e93299c0e2ae
Takaki Minami <southern@triple-es.com>
2023-11-15 19:52:55 +0900

    fix error.

--- components/LanguageCombo/index.tsx
+++ components/LanguageCombo/index.tsx
@@ -12,7 +12,7 @@ import { Command, CommandGroup, CommandItem } from "@/components/ui/command";
 import { languages } from "@/app/[lng]/i18n/settings";
 import Link from "next/link";
 import { useTranslation } from "@/app/[lng]/i18n/client";
-import { LanguageDetectorAsyncModule } from "i18next";
+import { useSearchParams, usePathname } from "next/navigation";
 
 const langSet = new Set(languages);
 const langs = [
@@ -31,9 +31,11 @@ const getLabel = (value: string): string | undefined =>
   langs.find((lang) => lang.value === value)?.label;
 
 const LanguageCombo = ({ params }: { params: { lng: string } }) => {
+  const path = usePathname();
+  const search = useSearchParams();
   const [open, setOpen] = React.useState(false);
-  const [value, setValue] = React.useState("");
   const { t } = useTranslation(params.lng);
+  const exceptLangPath = path.split("/").slice(2).join("/");
   return (
     <>
       <Popover open={open} onOpenChange={setOpen}>
@@ -44,13 +46,10 @@ const LanguageCombo = ({ params }: { params: { lng: string } }) => {
           <Command>
             <CommandGroup>
               {langs.map((lang) => {
-                const path = location.pathname.split("/").slice(2).join("/");
-                const search = location.search;
-                const hash = location.hash;
                 return (
                   langSet.has(lang.value) && (
                     <CommandItem key={lang.value} value={lang.value}>
-                      <Link href={`/${lang.value}/${path}${search}${hash}`}>
+                      <Link href={`/${lang.value}/${exceptLangPath}?${search}`}>
                         {lang.label}
                       </Link>
                     </CommandItem>
